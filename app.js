d98661d2db54ef8e1f67d344157632f4fe6adc8a
bborncr <bentley@crcibernetica.com>
2017-06-30 18:35:42 +0000

    added some cool stuff from the lecture

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = require("./credentials");
+var pass = require("./credentials").password;
 
 app.say('James');
 app.say2('Jaime');
 
 console.log(`Username is: ${app.username}`);
 console.log(`Password is: ${app.password}`);
+console.log(`The password is ${pass}`);
