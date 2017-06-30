883d5c177f0c96c78a3a68dc1599a41269ca006a
bborncr <bentley@crcibernetica.com>
2017-06-30 17:39:40 +0000

    added hidden function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require("./credentials");
 
 app.say('James');
+app.say2('Jaime');
 
 console.log(`Username is: ${app.username}`);
 console.log(`Password is: ${app.password}`);
--- credentials.js
+++ credentials.js
@@ -1,6 +1,11 @@
+var say2 = function exportedSay(name){
+  return console.log(`Bond...${name} Bond.`);
+}
+
 module.exports = {
   username:   'jamesbond',
   password:   'bullwinkle',
+  say2:       say2,
   say:        function(name){
   console.log(`Bond...${name} Bond.`);
   }
